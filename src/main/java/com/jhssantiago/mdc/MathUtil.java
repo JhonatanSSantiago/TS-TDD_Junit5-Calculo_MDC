d49e33051230dee41fd3d8e2720a1f5df0dda291
Jhonatan Santiago <jhonssantiago@gmail.com>
2021-05-22 12:02:06 -0300

    Testes das propriedades 2 a 7

--- src/main/java/com/jhssantiago/mdc/MathUtil.java
+++ src/main/java/com/jhssantiago/mdc/MathUtil.java
@@ -6,8 +6,25 @@ package com.jhssantiago.mdc;
  */
 public class MathUtil {
     public static int mdc(int a, int b){
-        if(b > 0 && a % b == 0){           
+        a = Math.abs(a); //P7
+        b = Math.abs(b);
+        
+        
+        final int maior = Math.max(a, b); //P6
+        b = Math.min(a, b);
+        a = maior;
+        
+       
+        if(b > 0 && a % b == 0){ // P1         
            return b;
+        }
+        
+        if(b == 0 ){ //P3
+            return Math.abs(a);
+        }
+        
+        if(a % b != 0){ //P5
+            return 1;
         }
         return -1;
     }
--- src/test/java/com/jhssantiago/mdc/MathUtilTest.java
+++ src/test/java/com/jhssantiago/mdc/MathUtilTest.java
@@ -2,6 +2,7 @@ package com.jhssantiago.mdc;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import static com.jhssantiago.mdc.MathUtil.mdc;
 
 /**
  *
@@ -9,24 +10,154 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 public class MathUtilTest {
 
+    /* Testes da Propriedade 1
+        Se B > 0 é um divisor de A, então MDC(A,B) = B.
+    */
     @Test
-    void testMdcAParP1() {
-    /* Propriedade 1 - Se B > 0 é um divisor de A, então MDC(A,B) = B. */
+    void testMdcAParP1() {       
         final int a = 6;
         final int b = 3;
-        final int esperado = b;       
+        final int esperado = b;
+        final int obtido = MathUtil.mdc(a, b);
+        assertEquals(esperado, obtido);
+    }
+
+    @Test
+    void testMdcAImparP1() {
+        final int a = 9;
+        final int b = 3;
+        final int esperado = b;
+        final int obtido = MathUtil.mdc(a, b);
+        assertEquals(esperado, obtido);
+    }
+    
+    /* Testes da Propriedade 2
+        Todo número que for divisor comum de A e B também é um divisor de MDC(A,B);
+    */
+    @Test
+    void testeMdcParesP2() {
+        final int a = 16;
+        final int b = 8;
+        final int divisor = 4;
+        final int obtido = MathUtil.mdc(a, b);
+        assertTrue(obtido % divisor == 0);
+    } 
+    
+    /* Testes da Propriedade 3
+       Considerando que todos os números são fatores de 0 (pois 0=0*A para qualquer A inteiro) então MDC(A, 0) = |a|;
+    */
+    @Test
+    void testeMdcAPositivoP3(){
+        final int a = 15;
+        final int b = 0;
+        final int esperado = 15;
         final int obtido = MathUtil.mdc(a, b);
         assertEquals(esperado, obtido);
     }
     
     @Test
-    void testMdcAImparP1() {
-        /* Propriedade 1 - Se B > 0 é um divisor de A, então MDC(A,B) = B. */
-        final int a = 9;
-        final int b = 3;
-        final int esperado = b;       
+    void testMdcANegativoP3() {
+        final int a = -30;
+        final int b = 0;
+        final int esperado = 30;
         final int obtido = MathUtil.mdc(a, b);
         assertEquals(esperado, obtido);
     }
     
+    /* Testes da Propriedade 4
+        Se M é um inteiro não negativo então MDC(M*A, M*B) = M*MDC(A,B);
+    */
+    
+    @Test
+    void testMdcMParP4(){
+        final int a = 9;
+        final int b = 3;
+        final int m = 2;
+        //mdc(a*m, b*m) == m * mdc(a, b)
+        //mdc(18, 6) == 2 * mdc (9, 3)
+        //      6   == 2 * 3
+        final int esperado = MathUtil.mdc(a*m, b*m);
+        final int obtido = m * MathUtil.mdc(a, b);
+        assertEquals(esperado, obtido);        
+    }
+    
+    @Test
+    void testMdcMImparP4(){
+        final int a = 9;
+        final int b = 3;
+        final int m = 3;
+        //mdc(a*m, b*m) == m * mdc(a, b)
+        //mdc(27, 9) == 3 * mdc (9, 3)
+        //      9   == 3 * 3
+        final int esperado = MathUtil.mdc(a*m, b*m);
+        final int obtido = m * MathUtil.mdc(a, b);
+        assertEquals(esperado, obtido);        
+    }
+    
+    /* Testes da Propriedade 5
+        Se MDC(A, B) = 1 então MDC(A*B, C) = MDC(A,C) * MDC(B, C);
+    */
+    
+     @Test
+    void testMdcP5(){
+       final int a = 7;
+       final int b = 3;
+       final int c = 5;
+       final int esperado = mdc(a*b, c);
+       final int obtido = mdc(b, c);
+       assertEquals(esperado, obtido);
+    }
+    
+    @Test
+    void testMdcP5Resultado1(){      
+       final int b = 3;
+       final int c = 5;
+       final int esperado = 1;
+       final int obtido = mdc(b, c);
+       assertEquals(esperado, obtido);
+    }
+    
+    /* Testes da Propriedade 6
+        MDC(A, B) = MDC(B, A);
+    */
+    
+    @Test
+    void testMdcP6(){
+        final int a = 8;
+        final int b = 2;      
+        final int esperado = mdc(a, b);
+        final int obtido = mdc(b, a);
+        assertEquals(esperado, obtido);
+    }
+    
+    @Test
+    void testMdcP6Negativo(){
+        final int a = -8;
+        final int b = -2;      
+        final int esperado = mdc(a, b);
+        final int obtido = mdc(b, a);
+        assertEquals(esperado, obtido);
+    }
+    
+    /* Testes da Propriedade 7
+        MDC(-A, B) = MDC(A, -B) = MDC(-A, -B) = mdc (A, B)
+    */
+    
+    @Test
+    void testMdcP7TudoNegativo(){
+        final int a = -12;
+        final int b = -6;
+        final int esperado = 6;
+        final int obtido = mdc(a, b);
+        assertEquals(esperado, obtido);
+    }
+    
+    @Test
+    void testMdcP7ANegativo(){
+        final int a = -12;
+        final int b = 6;
+        final int esperado = 6;
+        final int obtido = mdc(a, b);
+        assertEquals(esperado, obtido);
+    }
 }
